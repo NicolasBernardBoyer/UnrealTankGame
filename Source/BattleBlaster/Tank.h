aea59b310b15f75464f2bc59003be08ec7d32fa4
NicolasBernardBoyer <77691659+NicolasBernardBoyer@users.noreply.github.com>
2026-01-30 01:03:16 -0500

    First commit, movement of the tank works

---
+++ Source/BattleBlaster/Tank.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "GameFramework/SpringArmComponent.h"
+
+#include "InputAction.h"
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
+
+#include "EnhancedInputSubsystems.h"
+
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBLASTER_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	ATank();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	class UInputMappingContext* TankMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere)
+	float Speed = 300.0f;
+	
+	void MoveInput(const FInputActionValue& Value);
+};
